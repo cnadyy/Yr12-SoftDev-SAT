88c18bc038269e81d6302b7b2fdd817aa0523124
cnadcnad <liamwatcheshentai@gmail.com>
2023-07-27 11:35:06 +1000

    main
    continued work on main.py and fileDatabase.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-# harrison :3
+# harri
 # Main UI for Cable Management system
 # 25.7.23 - __.__.23
 
 import customtkinter
+import fileDatabase
+
+db = fileDatabase()
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title('Duffinator_CableManager7014')
         self.geometry('960x540')
+
+        db.removeCables()
 
 
 # class 
