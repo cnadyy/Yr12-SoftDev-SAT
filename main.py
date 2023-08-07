2046e08543c7fee39f226d92bd3ac83294b80b49
cnadcnad <liamwatcheshentai@gmail.com>
2023-08-07 23:37:44 +1000

    main
    idk

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Main UI for Cable Management system
 # 25.7.23 - __.__.23
 
+from tkinter import Label
 import customtkinter
 import fileDatabase
 
@@ -21,10 +22,17 @@ class ScrollingCables(customtkinter.CTkScrollableFrame):
         self.grid_columnconfigure(0, weight=1)
 
         cables = db.getCables()
-
-        i = 0
-        for cable in cables:
-            print(cable[0])
+        
+        print(cables)
+        # i = 0
+        # for cable in cables:
+        #     print(cable[i])
+        #     i += 1
+            
+            # cableObj = customtkinter.CTkLabel(self, text=cable)
+            # cableObj.grid(row=i, column=0, columnspan=100, sticky='ew')
+            # print(cable[0])
+            # i += 1
 
 
 
@@ -33,16 +41,41 @@ class ScrollingCables(customtkinter.CTkScrollableFrame):
         #print(db.getCables())
         #print('where is this maent to be')
 
+class HeaderFrame(customtkinter.CTkFrame):
+    def __init__(self, master, bg_color):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
 
+        self.lblTitle = customtkinter.CTkLabel(self, text='Duffinator_CableManager7014')
+        self.lblTitle.grid(row=0, column=0, padx=(10, 0))
+
+        def btnCallBack():
+            print('test')
+
+        self.btnAddCable = customtkinter.CTkButton(self, text='Add Cable', command=btnCallBack)
+        self.btnAddCable.grid(row=1, column=0, padx=(10, 0))
+
+        self.userSearch = customtkinter.CTkEntry(self)
+        self.userSearch.grid(row=0, column=1, padx=(10,0))
+
+        self.lblSort = customtkinter.CTkLabel(self, text='sort:')
+        self.lblSort.grid(row=0, column=2, padx=(10, 0))
+
+        self.optionSort = customtkinter.CTkOptionMenu(self, values=['option', 'date ^', 'date v', 'length ^', 'length v'])
+        self.optionSort.grid(row=0, column=3, padx=(10,0))
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title('Duffinator_CableManager7014')
         self.geometry('960x540')
+        self.grid_columnconfigure(0, weight=1)
+
+        self.appHeader = HeaderFrame(self, bg_color='green')
+        self.appHeader.grid(row=0, column=0, padx=(10, 0), pady=(10, 0), sticky='nsew')
 
         self.scrollingCables = ScrollingCables(self, title='test')
-        self.scrollingCables.grid(row=1, column=0, padx=(10, 0), pady=(10, 0), sticky="nsew")
+        self.scrollingCables.grid(row=1, column=0, padx=(10, 0), pady=(10, 0), sticky='nsew')
 
         db.sortCable()
 
